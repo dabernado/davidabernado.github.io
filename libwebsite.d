7b645ab0cb92f736c4c203fd578d85a06a0d7b6f
David Bernado <dbernado@protonmail.com>
2018-06-12 16:04:02 -0700

    added stuff

---
+++ libwebsite.d
@@ -0,0 +1,1 @@
+/home/david/Documents/Projects/website/target/wasm32-unknown-unknown/release/libwebsite.rlib: /home/david/Documents/Projects/website/src/lib.rs
---
+++ website.d
@@ -0,0 +1,1 @@
+/home/david/Documents/Projects/website/target/wasm32-unknown-unknown/release/website.wasm: /home/david/Documents/Projects/website/src/lib.rs /home/david/Documents/Projects/website/src/main.rs
